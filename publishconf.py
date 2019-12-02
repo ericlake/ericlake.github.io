073fb19d22b0f6830d8d5d932f812cf1e9dd9ac4
Eric Lake <ericlake@gmail.com>
2019-12-02 16:49:20 -0600

    repush first post

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://ericlake.github.io'
+SITEURL = 'https://etank.us'
 RELATIVE_URLS = False
 SITELOGO = SITEURL + '/static/avatar.png'
 FAVICON = SITEURL + '/static/favicon.ico'

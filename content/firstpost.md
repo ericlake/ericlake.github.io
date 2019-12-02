3281d87e79f03949c1f90ea841dc26d05cc0dc5f
Eric Lake <ericlake@gmail.com>
2019-12-02 16:26:21 -0600

    initial commit

---
+++ content/firstpost.md
@@ -0,0 +1,23 @@
+---
+Title: First Pelican Post
+Date: 2019-11-27 19:30
+Category: General
+Tags: pelican, publishing
+Slug: first-pelican-post
+Authors: Eric Lake
+---
+
+This is my inital post to test the integration between pelican and github pages. My intention is to use this for all future blogging assuming that I can get all the pieces to work together smoothly.
+
+Part of making this work will involve getting auto publishing on push to work as well as having a nice theme. Another thing that I want to have working is the automation of the metadata at the top of each of the markdown source files. Having to fill in the following data each time is kind of a pain:
+
+```
+Title: This one makes sense to fill in
+Date: Just use the current time
+Category: General or something
+Tags: maybe, it, makes, sense, for, these
+Slug: some-slug
+Authors: duh it's me
+Summary: Some short thing about the post
+```
+
---
+++ pelicanconf.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Eric Lake'
+SITENAME = 'Ramblings'
+SITETITLE = 'Ramblings'
+SITESUBTITLE = 'Ramblings of someone who should be more closely supervised!'
+SITEDESCRIPTION = 'Ramblings of someone who should be more closely supervised!'
+SITEURL = 'http://localhost:8000'
+SITESRC = 'https://github.com/ericlake/ericlake.github.io'
+SITELOGO = SITEURL + '/static/avatar.png'
+FAVICON = SITEURL + '/static/favicon.ico'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Biography
+BIO = "Site Reliability Engineer @LogDNA"
+PROFILE_IMAGE = 'avatar.png'
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/ericlake/'),
+          ('twitter', 'https://twitter.com/etank'),
+          ('github', 'https://github.com/ericlake'))
+
+DEFAULT_PAGINATION = 10
+
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# URL settings
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+
+# Theme
+THEME = 'themes/flex'
+
+# Plugins
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['neighbors', 'post_stats']
+
+# static files
+STATIC_PATHS = [
+    'static'
+]
+
+JINJA_ENVIRONMENT = {
+    'extensions': [
+        'jinja2.ext.do'
+    ]
+}
+
+COPYRIGHT_YEAR = 2019
+COPYRIGHT_NAME = 'Eric Lake'
+
+MAIN_MENU = True
+
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+LINKS = (('Archives', '/archives.html'),)
